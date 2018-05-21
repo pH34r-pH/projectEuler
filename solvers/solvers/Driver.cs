50be419a542ad82986e8ff94f20e9869e009f890
Tyler Harbin-Giuntoli <t-tyha@microsoft.com>
2018-05-21 13:26:49 -0700

    Initial commit & setup of project

---
+++ solvers/solvers/Driver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solvers
+{
+    class Driver
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of the problem to solve: ");
+            string Problem = "P" + Console.ReadLine();
+            Console.WriteLine("Solving...");
+            typeof(Problems).GetMethod(Problem).Invoke(null, null);
+            Console.WriteLine("Press the enter key to quit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ solvers/solvers/Problems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solvers
+{
+    public static class Problems
+    {
+        public static void P1()
+        {
+
+        }
+    }
+}
