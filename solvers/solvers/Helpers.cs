0860a36d9749f58407dd26f838ed065ec9074b4f
Tyler Harbin-Giuntoli <t-tyha@microsoft.com>
2018-05-21 13:46:42 -0700

    First two solves

---
+++ solvers/solvers/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solvers
+{
+    public static class Helpers
+    {
+        public static IEnumerable<int> FibonnaciNums()
+        {
+            var numbers = new List<int>(){ 0, 1 };
+            while (true)
+            {
+                numbers.Add(numbers[0] + numbers[1]);
+                numbers.RemoveAt(0);
+                yield return numbers[0];
+            }
+        }
+    }
+}
--- solvers/solvers/Problems.cs
+++ solvers/solvers/Problems.cs
@@ -8,9 +8,27 @@ namespace solvers
 {
     public static class Problems
     {
+        /// <summary>
+        /// Finds the sum of all numbers below 1000 that are a multiple of either 3 or 5.
+        /// </summary>
         public static void P1()
         {
+            int sum = 0;
+            for(int i = 0; i < 1000; ++i)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
 
+        /// <summary>
+        /// The sum of the even-numbered fibonacci numbers below four million
+        /// </summary>
+        public static void P2()
+        {
+            int sum = Helpers.FibonnaciNums().Where(x => x % 2 == 0).TakeWhile(c => c < 4000000).Sum();
+            Console.WriteLine("Sum: " + sum);
         }
     }
 }
